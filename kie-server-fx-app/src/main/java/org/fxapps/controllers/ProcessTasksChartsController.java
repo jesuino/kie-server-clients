b3c0286e051ce9a57c526025c5974fc19ba5b1cc
jesuino <william.fatecsjc@gmail.com>
2017-01-29 00:01:32 -0200

    Adding tasks by status

--- kie-server-fx-app/src/main/java/org/fxapps/controllers/ProcessTasksChartsController.java
+++ kie-server-fx-app/src/main/java/org/fxapps/controllers/ProcessTasksChartsController.java
@@ -13,6 +13,7 @@ import org.fxapps.service.KieServerClientService;
 import org.fxapps.utils.AppUtils;
 import org.kie.server.api.model.KieContainerResource;
 import org.kie.server.api.model.instance.ProcessInstance;
+import org.kie.server.api.model.instance.TaskSummary;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -29,13 +30,15 @@ public class ProcessTasksChartsController implements Initializable {
 	BarChart<String, Integer> chartPiByStatus;
 	@FXML
 	BarChart<String, Integer> chartPiByDate;
+	@FXML
+	BarChart<String, Integer> chartTasksByStatus;
 	
 	private KieServerClientService service;
 	private String containerId;
 
 	private List<ProcessInstance> allProcessInstances;
 	
-	SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyy hh:mm a");
+	SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyy\nhh:mm a");
 	
 
 	@Override
@@ -46,8 +49,6 @@ public class ProcessTasksChartsController implements Initializable {
 	}
 	
 	private void configure() {
-		chartPiByStatus.setLegendVisible(false);
-		chartPiByDate.setLegendVisible(false);
 	}
 
 	private void loadData() {
@@ -55,21 +56,30 @@ public class ProcessTasksChartsController implements Initializable {
 		
 		// TODO: do async
 		allProcessInstances = service.allProcessInstances(containerId, 100);
+		List<TaskSummary> allUserTasks = service.allUserTasks(100);
+		
 		Series<String, Integer>  piByStatusSeries = new Series<>();
 		Series<String, Integer>  piByDateSeries = new Series<>();
+		Series<String, Integer>  taskByStatusSeries = new Series<>();
 
 		allProcessInstances.stream()
 			.collect(Collectors.groupingBy(ProcessInstance::getState))
-			.forEach((k, v) -> piByStatusSeries.getData().add(new Data<String, Integer>(AppUtils.getStatusName(k), v.size()))
+			.forEach((k, v) -> piByStatusSeries.getData().add(new Data<>(AppUtils.getStatusName(k), v.size()))
 		);
 		
 		allProcessInstances.stream()
 		 	.collect(Collectors.groupingBy(pi -> dateFormat.format(pi.getDate())))
-		 	.forEach((k, v) -> piByDateSeries.getData().add(new Data<String, Integer>(k, v.size()))
+		 	.forEach((k, v) -> piByDateSeries.getData().add(new Data<>(k, v.size()))
+		 );
+		
+		allUserTasks.stream()
+			.collect(Collectors.groupingBy(TaskSummary::getStatus))
+			.forEach((k, v) -> taskByStatusSeries.getData().add(new Data<>(k, v.size()))
 		 );
 		
 		chartPiByStatus.getData().add(piByStatusSeries);
 		chartPiByDate.getData().add(piByDateSeries);
+		chartTasksByStatus.getData().add(taskByStatusSeries);
 		
 	}
 
